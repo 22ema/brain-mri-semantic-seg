a9f22facffcc512b59bd40db1ac627645d66b162
22ema <yunsung9503@gmail.com>
2019-06-15 16:08:01 +0900

    Update tumor_pre.py

--- image_preprocessing/tumor_pre.py
+++ image_preprocessing/tumor_pre.py
@@ -19,17 +19,17 @@ import os
 from flair_t1c_image_prepare import flair_t1c_image_prepare
 
 ###이미지 불러오기
-new_path = './image'
+new_path = './fin_flair/fin_flair_dataset'
 count= 0
 file_list=list()
 for image,_,files in os.walk(new_path):
     file_list = sorted(files)
 
 ## 이미지 binary_image 로 만들어 gray_data폴더에 저장
-for file in file_list:
+for file in file_list[:5]:
     img = cv2.imread(new_path+file,cv2.IMREAD_COLOR)
     img=flair_t1c_image_prepare.imagepre(img)
-    cv2.imwrite("test_t1c/"+file,img)
+    cv2.imwrite("test_flair_test/"+file,img)
 print("---------finish------")
     
 
